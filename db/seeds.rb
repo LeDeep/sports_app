74a6b7916bea547cafba1ff38103be952e5747f7
Deep Marreddy <smarreddy@cambriasolutions.com>
2018-09-20 11:56:32 -0700

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([
+  {first_name: 'Aaron', last_name: 'Rodgers', email: 'arodgers@goat.com', height_feet: 6, height_inches: 2, weight: 220},
+  {first_name: 'Drew', last_name: 'Brees', email: 'drew@brees.com', height_feet: 6, height_inches: 0, weight: 200},
+  {first_name: 'Philip', last_name: 'Rivers', email: 'philip@rivers.com', height_feet: 6, height_inches: 6, weight: 240},
+  {first_name: 'Matthew', last_name: 'Stafford', email: 'matthew@stafford.com', height_feet: 6, height_inches: 5, weight: 230}
+  ])
