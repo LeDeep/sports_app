42b962247e578d373f5ab404d008ef8a7df351dc
Deep Marreddy <smarreddy@cambriasolutions.com>
2018-09-20 13:00:46 -0700

    Get users from database and show in ui

--- app/javascript/components/user.js
+++ app/javascript/components/user.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
- class User extends Component {
+class User extends Component {
   render() {
     return(
-      <div>
-        <p>{this.props.name}</p>
+      <div className='user-index'>
+      <p>{this.props.first_name} {this.props.last_name}</p>
+      <p>{this.props.email}</p>
+      <p>{this.props.height_feet}'{this.props.height_inches} {this.props.weight}</p>
       </div>
     )
   }
 }
- export default User
+
+export default User
--- app/javascript/containers/App.js
+++ app/javascript/containers/App.js
@@ -3,13 +3,6 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import UserIndex from '../components/user-index';
 
-var userData = [
-  {name: 'Aaron Rodgers'},
-  {name: 'Drew Brees'},
-  {name: 'Philip Rivers'},
-  {name: 'Matthew Stafford'}
-]
-
 class App extends Component {
   constructor() {
     super();
@@ -19,9 +12,9 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-      users: userData
-    });
+    fetch('/api/v1/users.json').then(response => response.json())
+    .then((data) => {this.setState({users:data})
+    })
   };
 
   render () {
