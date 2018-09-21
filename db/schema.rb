a34efa2b0a52babf8333f8a3cc865834343a1633
Deep Marreddy <smarreddy@cambriasolutions.com>
2018-09-21 09:34:56 -0700

    Create team table and teams_users table for has_and_belongs_to_many relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180920182828) do
+ActiveRecord::Schema.define(version: 20180921161803) do
+
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams_users_tables", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "team_id"
+    t.index ["team_id"], name: "index_teams_users_tables_on_team_id"
+    t.index ["user_id"], name: "index_teams_users_tables_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
