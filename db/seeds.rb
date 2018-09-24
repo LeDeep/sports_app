c0fdeb1990f1f37c9d5d97fcca2107347cf2a3ba
Deep Marreddy <smarreddy@cambriasolutions.com>
2018-09-24 12:11:47 -0700

    Add images and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,4 +11,45 @@ users = User.create([
   {first_name: 'Drew', last_name: 'Brees', email: 'drew@brees.com', height_feet: 6, height_inches: 0, weight: 200},
   {first_name: 'Philip', last_name: 'Rivers', email: 'philip@rivers.com', height_feet: 6, height_inches: 6, weight: 240},
   {first_name: 'Matthew', last_name: 'Stafford', email: 'matthew@stafford.com', height_feet: 6, height_inches: 5, weight: 230}
-  ])
+])
+
+teams = Team.create([
+  {name: "Green Bay Packers"},
+  {name: "New Orleans Saints"},
+  {name: "San Diego Chargers"},
+  {name: "Detroit Lions"}
+])
+
+sports = Sport.create([
+  {name: "Football", icon: File.new(File.join(Rails.root, "/public/images/football.png"))},
+  {name: "Soccer", icon: File.new(File.join(Rails.root, "/public/images/soccer.png"))},
+  {name: "Basketball", icon: File.new(File.join(Rails.root, "/public/images/basketball.png"))},
+  {name: "Baseball", icon: File.new(File.join(Rails.root, "/public/images/baseball.png"))},
+  {name: "Tennis", icon: File.new(File.join(Rails.root, "/public/images/tennis.png"))},
+])
+
+memberships = Membership.create([
+  {user_id: 1, team_id: 1},
+  {user_id: 1, team_id: 2},
+  {user_id: 2, team_id: 1},
+  {user_id: 2, team_id: 2},
+  {user_id: 2, team_id: 3},
+  {user_id: 3, team_id: 3},
+  {user_id: 3, team_id: 4},
+  {user_id: 4, team_id: 2},
+  {user_id: 4, team_id: 4}
+])
+
+interests = Interest.create([
+  {user_id: 1, sport_id: 1},
+  {user_id: 1, sport_id: 2},
+  {user_id: 1, sport_id: 3},
+  {user_id: 1, sport_id: 4},
+  {user_id: 2, sport_id: 1},
+  {user_id: 2, sport_id: 2},
+  {user_id: 3, sport_id: 3},
+  {user_id: 3, sport_id: 4},
+  {user_id: 4, sport_id: 2},
+  {user_id: 4, sport_id: 3},
+  {user_id: 4, sport_id: 4},
+])
