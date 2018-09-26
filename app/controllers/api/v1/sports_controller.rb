230052e5c9de36bf3baa927c88b21f4710a2f932
Deep Marreddy <smarreddy@cambriasolutions.com>
2018-09-26 14:30:23 -0700

    Remove unused code

--- app/controllers/api/v1/sports_controller.rb
+++ app/controllers/api/v1/sports_controller.rb
@@ -4,12 +4,6 @@ class Api::V1::SportsController < ApplicationController
     render json: Sport.all
   end
 
-  def add_interest
-    @user = User.find(session[:user_id])
-    @sport = Sport.find(params[:id])
-    @user.sports << @sport
-  end
-
   private
 
   def sport_params
