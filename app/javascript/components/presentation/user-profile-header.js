c228063dbec44a53952416dde437f6ab7ca265f0
Deep Marreddy <smarreddy@cambriasolutions.com>
2018-09-27 15:11:29 -0700

    Add link to home/index page from profile page

--- app/javascript/components/presentation/user-profile-header.js
+++ app/javascript/components/presentation/user-profile-header.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react'
+import { Header, Button } from 'semantic-ui-react'
+import { Link } from "react-router-dom";
+
 
 class UserProfileHeader extends Component {
   render() {
     return(
       <div className='user-profile-header'>
+        <Button><Link to='/'>Home</Link></Button>
         <Header size='large'>{this.props.userInfo.first_name} {this.props.userInfo.last_name}'s Profile</Header>
       </div>
     )
